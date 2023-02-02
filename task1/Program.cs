a868e0ab96c98acbc8db9f78470d2da49787c7f2
Alen <Spookyshocky@ya.ru>
2023-02-02 16:05:55 +0100

    сделал дз под семинаром 5

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int sum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] array = GetArray(8, 100, 999);
+Console.Write($"[{String.Join(" ", array)}]");
+int count = sum(array);
+Console.WriteLine($" --> {count}");
---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int sum(int[] array)
+{
+    int count = 0;
+    int res = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        
+        res = res + array[i];
+        count++;
+        
+    }
+    return res;
+}
+
+int[] array = GetArray(8, 0, 99);
+Console.Write($"[{String.Join(" ", array)}]");
+int res =sum(array);
+Console.WriteLine($" --> {res}");
